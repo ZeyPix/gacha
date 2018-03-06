1394817593346c3040914acf26c5837732e6feb0
zey <benblaise930@gmail.com>
2018-03-06 19:37:24 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1520361360,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/roselia-sheet0.png",
+		"images/roselia-sheet1.png",
+		"images/moonbackgound-sheet0.png",
+		"images/dialogbox-sheet0.png",
+		"images/aestheticbackground-sheet0.png",
+		"images/machine-sheet0.png",
+		"images/draw_-sheet0.png",
+		"images/money-sheet0.png",
+		"images/common-sheet0.png",
+		"images/battle-sheet0.png",
+		"images/bossdeoufça-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
